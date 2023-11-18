4212adb9bcd6bf748c503a13b26cd74f1edb8a5b
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2023-11-18 23:02:56 +0900

    [Silver IV] Title: 문자열 집합, Time: 140 ms, Memory: 42628 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,8 +1,12 @@
-N,M = map(int,input().split()) 
-str_arr = [input() for _ in range(N + M)]
-str_obj = set()
-count = 0
+import sys
+input = sys.stdin.readline
 
+N,M = map(int,input().split()) # 집합에 포함되어있는 문자열의 개수는 N, 집합에 포함되어있는지 알아보는 문자열들의 개수는 M
+str_arr = [input() for _ in range(N + M)] # 문자열들을 배열 안에 넣는다.
+str_obj = set() # set으로 집합에 포함되어있는 문자열을 담는다.
+count = 0 # 두 번 이상인 집합에 포함되어있는 문자열의 개수를 저장할 변수 초기화
+
+# for문을 돌면서 N보다 작은 것은 집합에 포함되어있는 기준 문자열을 넣고 나머지는 기준 문자열과 중복이 되는지 확인 후 중복이 되면 카운트 + 1을 시켜준다.
 for i in range(N + M) :
   if N > i :
     str_obj.add(str_arr[i])

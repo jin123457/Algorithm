502927c48c1e4b0744127443913cceebeca42d1c
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2023-11-24 09:48:04 +0900

    [Silver II] Title: 베르트랑 공준, Time: 2004 ms, Memory: 123964 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,28 +1,30 @@
 import sys
 
-# 소수를 판별하는 함수 파라미터로 값을 받아서 2부터 nm의 루트 + 1만큼의 for문을 돈다.
-# 돌 때마다 j의 값이 1씩 추가가 되고, 만약 파라미터 % j가 0이 하나라도 있을 경우 그 수는 소수가 아니다.
+prime_list = [1] * (123456 * 2 + 1)
+prime_list[0] = 0
+prime_list[1] = 0
+ans = []
+# 소수를 판별하는 함수 파라미터로 N값을 받아서 N + 1부터 N * 2 + 1만큼의 for문을 돈다.
+# prime_list의 인덱스 값이 i가 되고, i가 0이 아닐 경우 다시 한 번 for문을 돌린다.
+# 두번째 for문을
 def is_prime(nm) :
-    for j in range(2,int(nm ** 0.5) +1) :
-        if nm % j == 0 :
-            return False
+    for i in range(nm + 1,nm * 2 + 1) :
+        if prime_list[i] :
+            for j in range(2,int(i**0.5) + 1) :
+                if i % j == 0 :
+                    prime_list[i] = 0
+                    break
         
-    return True
-ans = [] # 정답을 담을 리스트 선언
+    return prime_list
+
 # 0이 나올때까지 계속해서 반복을 해야하므로 while문을 선택했다.
 while True :
     N = int(sys.stdin.readline()) # N부터 2N까지의 소수를 몇 개인지 알아보기 위한 자연수를 N으로 받는다.
-    count = 0 # N부터 2N까지의 소수가 몇 개인지 세는 변수 선언
-    if N == 0 : break # 입력의 값이 0일 경우 while문은 종료된다.
-    # 입력의 값이 1,2일 경우 for문을 돌지 못하기 때문에 위에서 소수의 개수 1을 출력한다.
-    # 입력의 값이 1,2일 아닐 경우는 N + 1부터 2N + 1까지 돌면서 총 소수의 개수가 몇 개인지 파악한 후, for문이 끝날 때 개수를 정답 리스트에 담아준다.
-    if N in [1,2] :
-       ans.append(str(1))
+    # 입력의 값이 0일 경우 while문을 빠져나온다.
+    # 입력의 값이 0일 아닐 경우 is_prime 함수의 파라미터로 N을 넘겨주고 리턴 값으로 배열을 받는다.
+    if N == 0 : 
+        break
     else :
-        for i in range(N + 1,N * 2 + 1) :
-            if is_prime(i) :
-                count += 1
-        ans.append(str(count))
-        
-# 정답배열을 한칸씩 내려주면서 출력한다.
+      ans.append(str(sum(is_prime(N)[N + 1:2*N + 1])))
+
 print("\n".join(ans))

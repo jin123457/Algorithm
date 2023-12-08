3af00682c89165be3a5123cddf81aca08a0c921d
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2023-12-08 10:23:01 +0900

    [Silver III] Title: N과 M (8), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,16 @@
+def combinations() :
+    global N,M,ans,arr
+    if len(ans) == M :
+        print(" ".join(list(map(str,ans))))
+        return
+
+    for i in sorted(arr) :
+        if len(ans) >= 1 and ans[len(ans) - 1] > i :continue
+        ans += [i]
+        combinations()
+        ans.pop()
+
+N,M = map(int,input().split())
+arr = list(map(int,input().split()))
+ans = list()
+combinations()

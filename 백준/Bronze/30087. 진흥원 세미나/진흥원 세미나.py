f1218c5127af15d414542a151285b966f79db2fd
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2024-01-27 18:19:53 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,19 @@
+import sys
+
+nput = sys.stdin.readline
+
+obj = {
+  'Algorithm' : '204',
+  'DataAnalysis':'207',
+  'ArtificialIntelligence':'302',
+  'CyberSecurity':'B101',
+  'Network':'303',
+  'Startup':'501',
+  'TestStrategy':'105'
+}
+
+N = int(input())
+
+for _ in range(N) :
+  text = input().rstrip()
+  print(obj[text])

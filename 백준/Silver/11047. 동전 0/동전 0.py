e1e876d19786c6a95a42773bbe642da8c7e24c8d
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2024-02-21 20:23:01 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+arr = sorted([int(input()) for _ in range(N)],reverse=True)
+ans = 0
+
+for i in range(len(arr)) :
+  if not M : break
+  if arr[i] > M : continue
+  ans += M // arr[i]
+  M %= arr[i]
+  
+print(ans)

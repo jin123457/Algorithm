ca1c158f745b984f817eba52d69dca69b36d4134
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2024-03-07 16:30:58 +0900

    [Silver IV] Title: 암기왕, Time: 1108 ms, Memory: 304188 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -2,36 +2,18 @@ import sys
 
 input = sys.stdin.readline
 
-def BS(NN,s,e,m) :
-    is_find = False
-    while s < e :
-        mid = (s + e) // 2
-        
-        if NN[mid] > m : 
-            e = mid
-        elif NN[mid] < m :
-            s = mid + 1
-        elif NN[mid] == m :
-            is_find  = True
-            break
-
-    if is_find :
-        return '1'
-    else :
-        return '0'
-
-
 T = int(input())
 ans = list()
-
 for _ in range(T) :
     N = int(input())
-    NN = list(map(int,input().split()))
+    NN = set(map(int,input().split()))
     M = int(input())
     MM = list(map(int,input().split()))
-    
-    NN = sorted(NN)
+
     for m in MM :
-        ans.append(BS(NN,0,N,m))
+        if m in NN :
+            ans.append('1')
+        else :
+            ans.append('0')
 
 print("\n".join(ans))

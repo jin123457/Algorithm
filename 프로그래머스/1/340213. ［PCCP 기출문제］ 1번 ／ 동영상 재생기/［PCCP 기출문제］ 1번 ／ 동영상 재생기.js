240e0ddd5da1c48c69b3cfb68ffb1cdbabf64ff3
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2024-09-30 22:11:56 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,38 +1,32 @@
+function op_skip(pos,video_len,op_start,op_end){
+    if (pos > video_len) return video_len;
+    else if (pos < 0) return 0;
+    else if (op_start <= pos && pos <= op_end) return op_end;
+    
+    return pos;
+}
+
 function solution(video_len, pos, op_start, op_end, commands) {
     let answer = '';
     
-    let split_pos = pos.split(":");
-    let split_op_start = op_start.split(":");
-    let split_op_end = op_end.split(":");
-    let split_video_len = video_len.split(":");
+    let split_pos = pos.split(":").map(x => parseInt(x));
+    let split_op_start = op_start.split(":").map(x => parseInt(x));
+    let split_op_end = op_end.split(":").map(x => parseInt(x));
+    let split_video_len = video_len.split(":").map(x => parseInt(x));
 
-    if (parseInt(split_pos[0]) > 0)
-      pos = parseInt(split_pos[0]) * 60 + parseInt(split_pos[1]);
-    else pos = parseInt(split_pos[1]);
-
-    if (parseInt(split_op_start[0]) > 0)
-      op_start = parseInt(split_op_start[0]) * 60 + parseInt(split_op_start[1]);
-    else op_start = parseInt(split_op_start[1]);
-
-    if (parseInt(split_op_end[0]) > 0)
-      op_end = parseInt(split_op_end[0]) * 60 + parseInt(split_op_end[1]);
-    else op_end = parseInt(split_op_end[1]);
-
-    if (parseInt(split_video_len[0]) > 0)
-      video_len = parseInt(split_video_len[0]) * 60 + parseInt(split_video_len[1]);
-    else video_len = parseInt(split_video_len[1]);
+    pos = split_pos[0] * 60 + split_pos[1];
+    op_start = split_op_start[0] * 60 + split_op_start[1];
+    op_end = split_op_end[0] * 60 + split_op_end[1];
+    video_len = split_video_len[0] * 60 + split_video_len[1];
 
     for (let i = 0; i < commands.length; i++) {
-      if (pos > video_len) pos = video_len;
-      else if (pos < 0) pos = 0;
-      else if (op_start <= pos && pos <= op_end) pos = op_end;
-
-      if (commands[i] == "next") pos += 10;
-      else pos -= 10;
-
-      if (pos > video_len) pos = video_len;
-      else if (pos < 0) pos = 0;
-      else if (op_start <= pos && pos <= op_end) pos = op_end;
+      
+        pos = op_skip(pos,video_len,op_start,op_end)
+        
+        if (commands[i] == "next") pos += 10;
+        else pos -= 10;
+        
+        pos = op_skip(pos,video_len,op_start,op_end)
     }
 
     let hour = parseInt(pos / 60);

511f9859a55e87f41510c608c45ac8f74d88b7d4
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2024-10-04 18:50:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 236.55 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,26 +1,20 @@
 function solution(id_list, report, k) {
-    let recive_mail = id_list.reduce((info, name) => {
-  info[name] = 0;
-  return info;
-}, {});
-let reported_id = id_list.reduce((info, name) => {
-  info[name] = [];
-  return info;
-}, {});
+    let reports = [...new Set(report)].map((a) => {
+      return a.split(" ");
+    });
 
-for (let x of report) {
-  let [A, B] = x.split(" ");
-  if (!reported_id[B].includes(A)) {
-    reported_id[B].push(A);
-  }
-}
-
-for (let x of Object.keys(reported_id)) {
-  if (reported_id[x].length >= k) {
-    for (let c of reported_id[x]) {
-      recive_mail[c] += 1;
+    let counts = new Map();
+    for (const bad of reports) {
+      counts.set(bad[1], counts.get(bad[1]) + 1 || 1);
     }
-  }
-}
-    return Object.values(recive_mail);
+
+    let good = new Map();
+    for (const report of reports) {
+      if (counts.get(report[1]) >= k) {
+        good.set(report[0], good.get(report[0]) + 1 || 1);
+      }
+    }
+
+    let answer = id_list.map((a) => good.get(a) || 0);
+    return answer;
 }

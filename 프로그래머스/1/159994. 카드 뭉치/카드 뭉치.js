7f9602763faf2b12bd9818d831ca3e994b71790e
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2024-10-08 11:55:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,14 @@
+function solution(cards1, cards2, goal) {
+  let answer = "Yes";
+  for (let word of goal) {
+    if (word == cards1[0]) {
+      cards1.shift();
+    } else if (word == cards2[0]) {
+      cards2.shift();
+    } else {
+      answer = "No";
+      break;
+    }
+  }
+  return answer;
+}

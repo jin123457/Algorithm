9ddbe9f4f348a18701b2ebaf3f86f448c7fef9da
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2024-10-10 11:47:04 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 180.04 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,14 +1,11 @@
 function solution(s) {
   let answer = [-1];
   for (let i = 1; i < s.length; i++) {
-    let value = s[i];
+    let value = s.slice(0, [i]).split("").reverse().indexOf(s[i]);
     let near = -1;
-    for (let j = i - 1; j >= 0; j--) {
-      if (s[j] == value) {
-        near = i - j;
-        break;
-      }
-    }
+
+    if (value > -1) near = value + 1;
+
     answer.push(near);
   }
   return answer;

81fc95ba8f4cfa5ce5f2cf3ce04045189bc8ad3c
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2024-10-10 23:31:43 +0900

    [Silver III] Title: 숫자 야구, Time: 200 ms, Memory: 19056 KB -BaekjoonHub

---
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.js
@@ -0,0 +1,33 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let arr = input.slice(1).map((x) => x.split(" "));
+let answer = 0;
+for (let i = 123; i <= 987; i++) {
+  let is_find = true;
+  let my_num = String(i);
+  if (
+    my_num[0] == my_num[1] ||
+    my_num[0] == my_num[2] ||
+    my_num[1] == my_num[2] || +my_num[0] == 0 ||
+    +my_num[1] == 0 ||
+    +my_num[2] == 0
+  )
+    continue;
+  for (let x of arr) {
+    let [num, s, b] = x;
+    let strike = 0;
+    let ball = 0;
+    if (num[0] == my_num[0]) strike += 1;
+    if (num[1] == my_num[1]) strike += 1;
+    if (num[2] == my_num[2]) strike += 1;
+
+    if (my_num[0] == num[1] || my_num[0] == num[2]) ball += 1;
+    if (my_num[1] == num[0] || my_num[1] == num[2]) ball += 1;
+    if (my_num[2] == num[0] || my_num[2] == num[1]) ball += 1;
+
+    if (+s != strike || +b != ball) is_find = false;
+  }
+  if (is_find) answer += 1;
+}
+
+console.log(answer);

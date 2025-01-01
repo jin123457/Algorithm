79b73b08a774417ad916e3bcb35414c62ba7a9a8
sseo_dev <72060681+jin123457@users.noreply.github.com>
2025-01-01 14:38:49 +0900

    [level 1] Title: 햄버거 만들기, Time: 6302.97 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,33 @@
+function solution(ingredient) {
+  let answer = 0;
+  const arr = [];
+  let i = 0;
+  while(ingredient.length >= i) {
+    if(arr.length === 0 && ingredient[i] === 1 && ingredient[i + 1] === 2) {
+      arr.push(i);
+      i += 1;
+    } else if(arr.length === 1 && ingredient[i] === 2 && ingredient[i - 1] === 1) {
+      arr.push(i);
+      i += 1;
+    } else if(arr.length === 2 && ingredient[i] === 3 && ingredient[i - 1] === 2) {
+      arr.push(i);
+      i += 1;
+    } else if(arr.length === 3 && ingredient[i] === 1 && ingredient[i - 1] === 3) {
+      arr.push(i);
+      i += 1;
+    } else if(arr.length === 4) {
+      i = arr[0] - 4;
+      ingredient.splice(arr[0],4);
+      arr.length = 0;
+      answer += 1;
+    } else {
+      arr.length = 0;
+      if(arr.length === 0 && ingredient[i] === 1 && ingredient[i + 1] === 2) {
+        arr.push(i);
+      }
+      i += 1;
+    }
+  };
+
+  return answer;
+}

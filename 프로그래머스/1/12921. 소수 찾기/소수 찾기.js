ee0828369c82dc5c6c4fce9cb983b0f71cb7f7f2
seo_dev <72060681+jin123457@users.noreply.github.com>
2025-02-13 00:12:35 +0900

    [level 1] Title: 소수 찾기, Time: 240.74 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,14 @@
+function isPrime(num) {
+    for(let i = 2;i <= Math.floor(Math.sqrt(num));i++) {
+        if(num % i === 0) return false;
+    }
+    
+    return true;
+}
+function solution(n) {
+    let answer = 0;
+    for(let i = 2; i <= n;i++) {
+        if(isPrime(i)) answer ++;
+    }
+    return answer;
+}

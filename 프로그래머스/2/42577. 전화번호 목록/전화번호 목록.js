82ac3586bbdfe17112aa10a51529583290792d71
seo_dev <72060681+jin123457@users.noreply.github.com>
2025-03-22 22:49:44 +0900

    [level 2] Title: 전화번호 목록, Time: 133.74 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,10 @@
+function solution(phone_book) {
+    let answer = true;
+    phone_book.sort();
+    
+    for (let i = 1; i < phone_book.length; i++) {
+        if (phone_book[i].startsWith(phone_book[i - 1])) answer = false;
+    }
+
+    return answer;
+}
